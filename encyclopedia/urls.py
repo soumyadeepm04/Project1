d2833bf3fa14b3b5a7b9dc861b4afc9ddcc76717
soumyadeepm04 <bmahapatra@c02xk3umjgh5.myfiosgateway.com>
2021-06-01 17:21:06 -0400

    completed search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.getentry, name = "getentry")
+    path("wiki/<str:title>", views.getentry, name = "getentry"),
+    path("search", views.search, name = "search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from . import util
+import encyclopedia
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,4 +14,26 @@ def getentry(request, title):
         "entry":util.get_entry(title), "title":title
     })
     
+def search(request):
+    if util.get_entry(request.GET.get("q")) != None:
+        return render(request, "encyclopedia/getentry.html", {
+        "entry":util.get_entry(request.GET.get("q")), "title":request.GET.get("q")
+    })
+
+    else:
+        filenames = util.list_entries()
+        substr_list = []
+        for file in filenames:
+            if request.GET.get("q") in file.lower():
+                 substr_list.append(file)
+
+            else:
+                pass
+        if len(substr_list) == 0:
+            return render(request, "encyclopedia/no_such_entry.html")
+        
+        else:
+            return render(request, "encyclopedia/search.html", {
+                "substr_list":substr_list
+            })
 
